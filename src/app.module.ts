7bb94bf72b9f4a6134b8ef3c243b0124fca0f638
Oluwasegun <arpeiks@gmail.com>
2021-04-24 06:29:00 +0100

    added mysql database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
   providers: [AppService],
+  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      port: 3306,
+      type: 'mysql',
+      entities: [],
+      database: 'todo',
+      synchronize: true,
+      host: 'localhost',
+      username: 'root',
+      password: 'password',
+    }),
+  ],
 })
 export class AppModule {}
